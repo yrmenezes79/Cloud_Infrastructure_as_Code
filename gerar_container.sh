3035a4dd002f17089043a663dfa9074f800d8c85
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2024-05-09 15:01:09 -0300

    Update gerar_container.sh

--- gerar_container.sh
+++ gerar_container.sh
@@ -1,8 +1,17 @@
+VERSAO=$1
+if [[ -z $VERSAO  ]]
+        then
+                VER=latest
+else
+                VER=$VERSAO
+fi
 echo "Gerando imagem Ansible EDA"
-podman build -t eda-ansible .
-echo "Fim da geração da imagem"
+podman rm ansible
+podman build -t eda-ansible:$VER .
 echo "Executando a imagem"
-podman run -d -p 8000:8000 localhost/eda-ansible
+podman run -d -p 5000:5000 --name ansible localhost/eda-ansible:$VER
 podman ps
-echo "Executar um processo"
-curl -H 'Content-Type: application/json' -d "{\message\": \"localhost\"}" 127.0.0.1:5000/endpoint
+echo "Executar uma chamada"
+curl -H 'Content-Type: application/json' -d '{"message": "localhost"}' 127.0.0.1:5000/endpoint
+podman logs -f ansible
+
