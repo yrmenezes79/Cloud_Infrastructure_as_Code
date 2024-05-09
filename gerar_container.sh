7d28b9a0d740d21cf15a6de32165399e6a485183
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2024-05-09 11:34:01 -0300

    Update gerar_container.sh

--- gerar_container.sh
+++ gerar_container.sh
@@ -5,4 +5,4 @@ echo "Executando a imagem"
 podman run -d -p 8000:8000 localhost/eda-ansible
 podman ps
 echo "Executar um processo"
-curl -H 'Content-Type: application/json' -d "{\message\": \"localhost\"}" 127.0.0.1:8000/endpoint
+curl -H 'Content-Type: application/json' -d "{\message\": \"localhost\"}" 127.0.0.1:5000/endpoint
