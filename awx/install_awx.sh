0eac4216b80052edc468a32bd2760ba0deef0adc
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2024-06-07 13:53:48 -0300

    Update install_awx.sh

--- awx/install_awx.sh
+++ awx/install_awx.sh
@@ -12,11 +12,6 @@ check_return_code() {
 
 FASE="Repositorio Docker"
 echo "$(date '+%Y-%m-%d %H:%M:%S') - $FASE"
-
-check_return_code "$FASE"
-
-FASE=""
-echo "$(date '+%Y-%m-%d %H:%M:%S') - $FASE"
 echo "[docker-ce-stable]
 name=Docker CE Stable - \$basearch
 baseurl=https://download.docker.com/linux/rhel/9/\$basearch/stable
@@ -31,11 +26,17 @@ dnf install git net-tools unzip wget python3 python3-pip ansible-core -y
 check_return_code "$FASE"
 dnf install docker-ce --nobest -y
 check_return_code "$FASE"
-
 dnf install docker-ce --nobest 
+check_return_code "$FASE"
 ansible-galaxy collection install community.docker
+check_return_code "$FASE"
 sed -i 's/, ssl_version=tls_version//g' /usr/local/lib/python3.9/site-packages/compose/cli/docker_client.py
-git clone https://github.com/yrmenezes79/Cloud_Infrastructure_as_Code.git
-cd Cloud_Infrastructure_as_Code/awx/
+check_return_code "$FASE"
+
+FASE="decompactacao pacote AWX"
+echo "$(date '+%Y-%m-%d %H:%M:%S') - $FASE"
 unzip awx-17.1.0.zip
 cd awx-17.1.0/installer/
+check_return_code "$FASE"
+
+
