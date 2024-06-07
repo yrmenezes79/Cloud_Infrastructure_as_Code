16879e2d594eda9150f5e88e282c541fe9931068
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2024-06-07 15:17:28 -0300

    Update install_awx.sh

--- awx/install_awx.sh
+++ awx/install_awx.sh
@@ -33,10 +33,31 @@ check_return_code "$FASE"
 sed -i 's/, ssl_version=tls_version//g' /usr/local/lib/python3.9/site-packages/compose/cli/docker_client.py
 check_return_code "$FASE"
 
+FASE="Start Docker"
+echo "$(date '+%Y-%m-%d %H:%M:%S') - $FASE"
+systemctl start docker
+check_return_code "$FASE"
+systemctl enable docker
+check_return_code "$FASE"
+
 FASE="decompactacao pacote AWX"
 echo "$(date '+%Y-%m-%d %H:%M:%S') - $FASE"
 unzip awx-17.1.0.zip
 cd awx-17.1.0/installer/
 check_return_code "$FASE"
 
+FASE="Alteracao arquivo inventory"
+echo "$(date '+%Y-%m-%d %H:%M:%S') - $FASE"
+sed -i 's/# awx_official=false/awx_official=true/g' inventory
+check_return_code "$FASE"
+sed -i 's|docker_compose_dir="~/.awx/awxcompose"|docker_compose_dir=/var/lib/awx|' inventory
+check_return_code "$FASE"
+sed -i 's|#project_data_dir=/var/lib/awx/projects|project_data_dir=/var/lib/awx/projects|' inventory
+check_return_code "$FASE"
+sed -i 's|# admin_password=password|admin_password=password|' inventory
+check_return_code "$FASE"
 
+FASE="Instalacao Ansible AWX"
+echo "$(date '+%Y-%m-%d %H:%M:%S') - $FASE"
+ansible-playbook -i inventory install.yml 
+check_return_code "$FASE"
