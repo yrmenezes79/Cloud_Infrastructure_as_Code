44fc847f0f840e3013e1d338d83bf35b14ecd228
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2024-06-20 14:18:05 -0300

    Update exec_template.sh

--- awx/exec_template.sh
+++ awx/exec_template.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Configurações
-AWX_SERVER="IP"
-USERNAME="admin"
-PASSWORD=""
+AWX_SERVER="awx.example.com"
+USERNAME="your_username"
+PASSWORD="your_password"
 TEMPLATE_NAME="projeto1"
 
 check_return_code() {
@@ -27,21 +27,23 @@ JOB_TEMPLATE_ID=$(curl -H "Authorization: Bearer $TOKEN" -X GET "http://$AWX_SER
 check_return_code
 
 FASE="Iniciar o Template de Job"
-curl -H "Authorization: Bearer $TOKEN" -X POST "http://$AWX_SERVER/api/v2/job_templates/$JOB_TEMPLATE_ID/launch/" -H "Content-Type: application/json"
+response=$(curl -H "Authorization: Bearer $TOKEN" -X POST "http://$AWX_SERVER/api/v2/job_templates/$JOB_TEMPLATE_ID/launch/" -H "Content-Type: application/json")
+job_id=$(echo $response | jq -r .job)
 check_return_code
 
+# Monitorar o Status do Job
 while true; do
   status=$(curl -H "Authorization: Bearer $TOKEN" -X GET "http://$AWX_SERVER/api/v2/jobs/$job_id/" -H "Content-Type: application/json" | jq -r .status)
   echo "Current job status: $status"
   
   if [[ "$status" == "successful" ]]; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') Job completed successfully."
+    echo "Job completed successfully."
     break
   elif [[ "$status" == "failed" ]]; then
-    echo "$(date '+%Y-%m-%d %H:%M:%S') Job failed."
+    echo "Job failed."
     break
   else
-    echo "Job rodando. Aguarde..."
+    echo "Job is $status..."
     sleep 10  # Aguarde 10 segundos antes de verificar novamente
   fi
 done
