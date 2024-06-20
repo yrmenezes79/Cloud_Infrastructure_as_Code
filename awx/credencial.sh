f199931ec3e2898b6b71065cdd9ff9a46df5f2ea
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2024-06-20 14:06:54 -0300

    Update credencial.sh

--- awx/credencial.sh
+++ awx/credencial.sh
@@ -3,11 +3,25 @@ USER=admin
 PASSWORD=
 URL="http://IP"
 ARQUIVO=response.json
+check_return_code() {
+    local return_code=$?
+    local message=$1
 
-# Obter Token de Autenticação
+    if [ $return_code -ne 0 ]; then
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Erro: $message (Código de retorno: $return_code)"
+        exit $return_code
+    else
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Sucesso: $message"
+    fi
+}
+
+FASE="Obter Token de Autenticação"
 TOKEN=$(curl -u $USER:$PASSWORD -X POST $URL/api/v2/tokens/ | jq -r '.token')
+check_return_code
 
-# Consultar Credenciais
+FASE="Consultar Credenciais"
+# 
 curl -H "Authorization: Bearer $TOKEN" $URL/api/v2/credentials/ > $ARQUIVO
+check_return_code
 
 cat $ARQUIVO |jq
