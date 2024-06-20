e96f17bc5ec7249d64d7976ee579d766d4ee5303
yrmenezes79 <88729005+yrmenezes79@users.noreply.github.com>
2024-06-20 14:07:51 -0300

    Update exec_template.sh

--- awx/exec_template.sh
+++ awx/exec_template.sh
@@ -6,13 +6,27 @@ USERNAME="admin"
 PASSWORD=""
 TEMPLATE_NAME="projeto1"
 
-# Obter Token de Autenticação
+check_return_code() {
+    local return_code=$?
+    local message=$1
+
+    if [ $return_code -ne 0 ]; then
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Erro: $message (Código de retorno: $return_code)"
+        exit $return_code
+    else
+        echo "$(date '+%Y-%m-%d %H:%M:%S') - Sucesso: $message"
+    fi
+}
+
+FASE="Obter Token de Autenticação"
 TOKEN=$(curl -u "$USERNAME:$PASSWORD" -X POST "http://$AWX_SERVER/api/v2/tokens/" -H "Content-Type: application/json" | jq -r .token)
+check_return_code
 
-# Obter ID do Template de Job
+FASE="Obter ID do Template de Job"
 JOB_TEMPLATE_ID=$(curl -H "Authorization: Bearer $TOKEN" -X GET "http://$AWX_SERVER/api/v2/job_templates/" -H "Content-Type: application/json" | jq -r ".results[] | select(.name==\"$TEMPLATE_NAME\") | .id")
+check_return_code
 
-# Iniciar o Template de Job
+FASE="Iniciar o Template de Job"
 curl -H "Authorization: Bearer $TOKEN" -X POST "http://$AWX_SERVER/api/v2/job_templates/$JOB_TEMPLATE_ID/launch/" -H "Content-Type: application/json"
+check_return_code
 
-echo "Template $TEMPLATE_NAME iniciado com sucesso."
